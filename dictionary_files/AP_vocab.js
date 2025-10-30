ee8e8d97de331884566adf86ac515a6d32ef8f33
clovell <clovell@gmail.com>
2025-10-30 19:02:00 -0400

    will a relative path work?

--- dictionary_files/AP_vocab.js
+++ dictionary_files/AP_vocab.js
@@ -363,7 +363,7 @@
         }
         
         try {
-            const response = await fetch('https://clovell.github.io/dictionary_files/AP_vocab.csv');
+            const response = await fetch('dictionary_files/AP_vocab.csv');
             if (!response.ok) { throw new Error(`HTTP error! status: ${response.status}`); }
             const csvText = await response.text();
             vocabulary = parseCSV(csvText);
